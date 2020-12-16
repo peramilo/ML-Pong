1a7a719f9e02775e11764d664df3d474965dae88
peramilo <61516266+peramilo@users.noreply.github.com>
2020-12-16 17:43:39 +0100

    Add files via upload

---
+++ envir.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pygame
+
+
+class Game:
+    def __init__(self):
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.screenWidth = 800
+        self.screenHeight = 600
+        self.padWidth = 100
+        self.padHeight = 15
+        self.ballRad = 12
+        self.padPosX = 0
+        self.padPosY = self.screenHeight - 10
+        self.padPosX = int(self.screenWidth / 2 - self.padWidth / 2)
+        self.ballPosX = int(self.screenWidth / 2 - self.ballRad / 2)
+        self.ballPosY = int(self.screenHeight / 6)
+        self.padVel = 0
+        self.ballVelX = int(np.random.choice((-5, -4, -3, 3, 4, 5), 1))
+        self.ballVelY = 4
+        self.point = 0
+        self.reward = 0
+        self.done = False
+        # For displaying attempts we need to uncomment screen and allow draw_screen in main_loop
+        # Using Clock we can limit the fps and make it easier to see model's actions
+        # self.fps = pygame.time.Clock()
+        # self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
+
+    def draw_screen(self):
+        self.screen.fill(self.BLACK)
+        self.draw_ball()
+        self.draw_pad()
+        pygame.display.flip()
+
+    def draw_ball(self):
+        pygame.draw.circle(self.screen, self.WHITE, (self.ballPosX, self.ballPosY), self.ballRad, 0)
+
+    def draw_pad(self):
+        pygame.draw.rect(self.screen, self.WHITE, (self.padPosX, self.padPosY, self.padWidth, self.padHeight))
+
+    def move_ball(self):
+        self.ballPosX += self.ballVelX
+        self.ballPosY += self.ballVelY
+
+    def move_pad(self):
+        if 0 <= self.padPosX <= self.screenWidth - self.padWidth:
+            self.padPosX += self.padVel
+        else:
+            if self.padPosX <= 0:
+                self.padPosX = 1
+            else:
+                self.padPosX = self.screenWidth - self.padWidth - 1
+        self.padVel = 0
+
+    def action(self, x):
+        if x == 1:
+            self.padVel += 4
+        else:
+            if x == 0:
+                self.padVel -= 4
+
+    def coll(self):
+        if self.ballPosY - self.ballRad <= 0:
+            self.ballPosY = self.ballRad
+            self.ballVelY *= -1
+        if self.ballPosX + self.ballRad >= self.screenWidth or self.ballPosX - self.ballRad <= 0:
+            self.ballVelX *= -1
+        if self.ballPosY + self.ballRad >= self.screenHeight:
+            if self.ballPosX + self.ballRad >= self.padPosX and self.ballPosX - self.ballRad <= self.padPosX + self.padWidth:
+                self.pad_coll()
+            else:
+                self.reward = -1000
+                self.done = True
+
+        if self.ballPosX + self.ballRad >= self.padPosX and self.ballPosX - self.ballRad <= self.padPosX + self.padWidth:
+            if self.ballPosY + self.ballRad >= self.padPosY:
+                self.pad_coll()
+
+    def pad_coll(self):
+        self.ballVelY *= -1
+        self.point += 1
+        self.reward = 200
+        self.ballPosY = self.padPosY - self.ballRad
+
+    def state(self):
+        x = (self.ballPosX / self.screenWidth, self.ballPosY / self.screenHeight,
+             (self.padPosX + self.padWidth / 2) / self.screenWidth)
+        return x
+
+    def reset(self):
+        self.__init__()
+        self.ballVelX = int(np.random.choice((-4, -3, 3, 4), 1))
+        self.ballVelY = 4
+        self.done = False
+        self.point = 0
+
+    def get_reward(self):
+        x = self.reward
+        self.reward = 0
+        return x
+
+    def is_done(self):
+        return self.done
+
+    def main_loop(self):
+        self.coll()
+        self.move_ball()
+        # Uncomment for limiting fps
+        # self.fps.tick(30)
+        self.move_pad()
+        # Uncomment for displaying attempts
+        # self.draw_screen()
+
---
+++ main.py
@@ -0,0 +1,120 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from envir import Game
+
+a = 1
+x = Game()
+max_steps = 4000
+max_history = 20000
+gamma = 0.99
+epsilon = 1.0
+epsilon_min = 0.01
+epsilon_decay = 0.995
+batch_size = 32
+episode_reward = 0
+episode_reward_history = []
+point_history = []      # Histories hold information from previous frames, decisions
+action_history = []
+state_history = []
+state_next_history = []
+done_history = []
+reward_history = []
+update_after_actions = 4  # Specifies how often to update our model.
+update_target_model = 10000
+
+
+def create_model():
+    inputs = layers.Input(shape=(3,))
+    layer1 = layers.Dense(128, activation="relu")(inputs)
+    action = layers.Dense(2, activation="softmax")(layer1)
+
+    return keras.Model(inputs=inputs, outputs=action)
+
+
+model = create_model()  # This is the main, decision making model
+model.summary()
+model_target = create_model()  # Target model, helps with learning
+optimizer = keras.optimizers.Adam(learning_rate=0.00025, clipnorm=1.0)
+loss_function = keras.losses.Huber()
+episode_number = 0
+
+while True:
+    x.reset()
+    state = x.state()
+    frame = 0
+    episode_reward = 0
+    for step in range(1, max_steps):
+        frame += 1
+        if np.random.rand() < epsilon:
+            action = np.random.choice(2)
+        else:
+            stateTensor = tf.convert_to_tensor(state)
+            stateTensor = tf.expand_dims(stateTensor, 0)
+            action_probs = model(stateTensor, training=False)
+            action = tf.argmax(action_probs[0]).numpy()
+
+        if epsilon > epsilon_min:
+            epsilon *= epsilon_decay
+
+        x.main_loop()
+        x.action(action)
+        done = x.is_done()
+        reward = x.get_reward()
+        state_next = x.state()
+        episode_reward += reward
+        action_history.append(action)
+        state_history.append(state)
+        state_next_history.append(state_next)
+        reward_history.append(reward)
+        done_history.append(done)
+        state = state_next
+
+        if frame % update_after_actions == 0 and len(done_history) > batch_size:
+            # Chooses random samples from our history:
+            indices = np.random.choice(range(len(done_history)), size=batch_size)
+            state_sample = np.array([state_history[i] for i in indices])
+            state_next_sample = np.array([state_next_history[i] for i in indices])
+            action_sample = np.array([action_history[i] for i in indices])
+            reward_sample = np.array([reward_history[i] for i in indices])
+            done_sample = tf.convert_to_tensor([float(done_history[i]) for i in indices])
+            future_rewards = model_target.predict(state_next_sample)
+            updated_q_values = reward_sample + gamma * tf.reduce_max(future_rewards, axis=1)
+            updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+            masks = tf.one_hot(action_sample, 2)
+
+            with tf.GradientTape() as tape:
+                q_values = model(state_sample)
+                q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+                loss = loss_function(updated_q_values, q_action)
+
+            # Backpropagation
+            grads = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        # Updating target model with main model weights
+        if frame % update_target_model == 0:
+            model_target.set_weights(model.get_weights)
+
+        # Deletes a row if we step over history limit
+        if len(reward_history) > max_history:
+            del reward_history[:1]
+            del state_history[:1]
+            del state_next_history[:1]
+            del action_history[:1]
+            del done_history[:1]
+
+        if x.is_done():
+            break
+    episode_number += 1
+    episode_reward_history.append(episode_reward)
+    point_history.append(x.point)
+    if len(episode_reward_history) > 10:
+        del episode_reward_history[:1]
+        del point_history[:1]
+    mean_reward = np.mean(episode_reward_history)
+    mean_point = np.mean(point_history)
+
+    if episode_number % 5 == 0:
+        print(episode_number, mean_reward, mean_point)
